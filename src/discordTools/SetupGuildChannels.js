d68158e4e976c4336ac5535dd911229f08d6ffc5
LordYk <70542339+LordYk@users.noreply.github.com>
2026-06-01 23:23:42 +0200

    Update copyright notice and improve comments

--- src/discordTools/SetupGuildChannels.js
+++ src/discordTools/SetupGuildChannels.js
@@ -1,23 +1,24 @@
 /*
-    Copyright (C) 2022 Alexander Emanuelsson (alexemanuelol)
+Copyright (C) 2022 Alexander Emanuelsson (alexemanuelol)
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-    https://github.com/alexemanuelol/rustplusplus
+https://github.com/alexemanuelol/rustplusplus
 
 */
 
+const { ChannelType } = require('discord.js');
 const DiscordTools = require('../discordTools/discordTools.js');
 const PermissionHandler = require('../handlers/permissionHandler.js');
 
@@ -41,9 +42,28 @@ async function addTextChannel(name, idName, client, guild, parent, permissionWri
     const instance = client.getInstance(guild.id);
 
     let channel = undefined;
+
+    /* Сначала ищем по сохранённому ID */
     if (instance.channelId[idName] !== null) {
         channel = DiscordTools.getTextChannelById(guild.id, instance.channelId[idName]);
     }
+
+    /* Если не нашли по ID — ищем по имени среди существующих каналов в этой категории */
+    if (channel === undefined) {
+        channel = guild.channels.cache.find(
+            ch => ch.name === name &&
+                  ch.type === ChannelType.GuildText &&
+                  ch.parentId === parent.id
+        );
+
+        if (channel !== undefined) {
+            /* Нашли существующий канал — сохраняем его ID */
+            instance.channelId[idName] = channel.id;
+            client.setInstance(guild.id, instance);
+        }
+    }
+
+    /* Если вообще не нашли — создаём */
     if (channel === undefined) {
         channel = await DiscordTools.addTextChannel(guild.id, name);
         instance.channelId[idName] = channel.id;
@@ -69,17 +89,12 @@ async function addTextChannel(name, idName, client, guild, parent, permissionWri
     }
 
     const perms = PermissionHandler.getPermissionsReset(client, guild, permissionWrite);
-
     try {
         await channel.permissionOverwrites.set(perms);
     }
     catch (e) {
         /* Ignore */
     }
-
-    /* Currently, this halts the entire application... Too lazy to fix...
-       It is possible to just remove the channels and let the bot recreate them with correct name language */
-    //channel.setName(name);
 
     channel.lockPermissions();
 }
