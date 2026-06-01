b71adb8e6afbf6106fbb03d9b5c619b2c69f6881
LordYk <70542339+LordYk@users.noreply.github.com>
2026-06-02 00:42:54 +0200

    Modify server list setup to clear channel conditionally
    Clear the text channel only on the first run of the setup.

--- src/discordTools/SetupServerList.js
+++ src/discordTools/SetupServerList.js
@@ -24,7 +24,10 @@ const DiscordTools = require('./discordTools.js');
 module.exports = async (client, guild) => {
     const instance = client.getInstance(guild.id);
 
-    await DiscordTools.clearTextChannel(guild.id, instance.channelId.servers, 100);
+    /* Очищаем канал только при первом запуске, иначе просто обновляем существующие сообщения */
+    if (instance.firstTime) {
+        await DiscordTools.clearTextChannel(guild.id, instance.channelId.servers, 100);
+    }
 
     for (const serverId in instance.serverList) {
         await DiscordMessages.sendServerMessage(guild.id, serverId);
