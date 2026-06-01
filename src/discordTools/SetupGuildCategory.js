8480f7457a15caf09dad57f369bcbda4ec050dd3
LordYk <70542339+LordYk@users.noreply.github.com>
2026-06-02 00:41:49 +0200

    Refactor category retrieval and creation logic

--- src/discordTools/SetupGuildCategory.js
+++ src/discordTools/SetupGuildCategory.js
@@ -1,29 +1,29 @@
 /*
-Copyright (C) 2022 Alexander Emanuelsson (alexemanuelol)
+    Copyright (C) 2022 Alexander Emanuelsson (alexemanuelol)
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-https://github.com/alexemanuelol/rustplusplus
+    https://github.com/alexemanuelol/rustplusplus
 
 */
 
-const { ChannelType } = require('discord.js');
 const DiscordTools = require('../discordTools/discordTools.js');
 const PermissionHandler = require('../handlers/permissionHandler.js');
 
 module.exports = async (client, guild) => {
     const instance = client.getInstance(guild.id);
+    const categoryName = 'Rinfo';
 
     let category = undefined;
 
@@ -32,27 +32,25 @@ module.exports = async (client, guild) => {
         category = DiscordTools.getCategoryById(guild.id, instance.channelId.category);
     }
 
-    /* Если не нашли по ID — ищем по имени среди существующих каналов */
+    /* Если не нашли по ID — ищем по названию (после редеплоя на Railway) */
     if (category === undefined) {
-        category = guild.channels.cache.find(
-            ch => ch.name === 'Rinfo' && ch.type === ChannelType.GuildCategory
-        );
-
+        category = DiscordTools.getCategoryByName(guild.id, categoryName);
         if (category !== undefined) {
-            /* Нашли существующую категорию — сохраняем её ID */
+            /* Нашли существующую — сохраняем её ID */
             instance.channelId.category = category.id;
             client.setInstance(guild.id, instance);
         }
     }
 
-    /* Если вообще не нашли — создаём */
+    /* Если вообще нет — создаём новую */
     if (category === undefined) {
-        category = await DiscordTools.addCategory(guild.id, 'Rinfo');
+        category = await DiscordTools.addCategory(guild.id, categoryName);
         instance.channelId.category = category.id;
         client.setInstance(guild.id, instance);
     }
 
     const perms = PermissionHandler.getPermissionsReset(client, guild, false);
+
     try {
         await category.permissionOverwrites.set(perms);
     }
